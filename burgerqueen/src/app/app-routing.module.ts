14dc610a658b99630e9331c21ed95d979f46320d
Sharon Huaman <sharonb.huaman@gmail.com>
2021-01-04 21:18:04 -0500

    add route

--- burgerqueen/src/app/app-routing.module.ts
+++ burgerqueen/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WaiterComponent} from './components/waiter/waiter.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'waiter', component: WaiterComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
